a4c651b1a3f4100353575d01087971428c7f5df0
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2025-01-25 08:54:19 +0100

    WIP radau.

--- dae4py/radau/radau.h
+++ dae4py/radau/radau.h
@@ -39,6 +39,8 @@ typedef void radau_jac_t(double *t, double *y, double *ydot,
 typedef void radau_mas_t(double *t, double *y, double *ydot, 
                          double *J, double* cj, 
                          double *rpar, F_INT *ipar);
+typedef void radau_mas_t(F_INT *neq, double *am, F_INT *lmas,
+                         double *rpar, F_INT *ipar);
 typedef void radau_solout_t(double *t, double *y, double *ydot, 
                             double *J, double* cj, 
                             double *rpar, F_INT *ipar);
@@ -60,31 +62,38 @@ void RADAU(F_INT *neq, radau_f_t *f, double *t,
            double *rwork, F_INT *lwork, F_INT *iwork, F_INT *liwork, 
            double *rpar, F_INT *ipar, F_INT *idid);
 
-
-// TODO: Perform the transformation
-// u' = v
-// 0 = f(t, u, v)
-void radau_f(F_INT *neq, double *t, double *y, 
+// f(t, y):
+//      u' = v
+//      0 = f(t, u, v)
+void radau_f(F_INT *neqn, double *t, double *y, 
             double *f, double *rpar, F_INT *ipar)
 {
-    PyObject *y_obj = NULL;
-    PyObject *yp_obj = NULL;
+    // python objects
+    PyObject *u_obj = NULL;
+    PyObject *v_obj = NULL;
     PyObject *result = NULL;
     PyObject *arglist = NULL;
+    PyArrayObject *v_array = NULL;
     PyArrayObject *result_array = NULL;
 
+    // dimension of implicit differential equation since y = (u, v)
+    F_INT n = *neqn / 2;
     npy_intp dims[1];
-    dims[0] = global_radau_params.neqn;
+    dims[0] = n;
 
-    /* Build numpy arrays from y and yp. */
-    y_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, y);
-    if (y_obj == NULL) {
-        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, y) failed.");
+    // decompose y = (u, v)
+    double *u = y;
+    double *v = y + n;
+
+    /* Build numpy arrays from u and v. */
+    u_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, u);
+    if (u_obj == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, u) failed.");
         goto fail;
     }
-    yp_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, yp);
-    if (yp_obj == NULL) {
-        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, yp) failed.");
+    v_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, v);
+    if (v_obj == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, v) failed.");
         goto fail;
     }
 
@@ -92,8 +101,8 @@ void radau_f(F_INT *neq, double *t, double *y,
     arglist = Py_BuildValue(
         "dOO",
         *t,
-        y_obj,
-        yp_obj
+        u_obj,
+        v_obj
     );
     if (arglist == NULL) {
         PyErr_SetString(PyExc_ValueError, "Py_BuildValue failed.");
@@ -107,21 +116,30 @@ void radau_f(F_INT *neq, double *t, double *y,
         goto fail;
     }
 
-    /* Build numpy array from result and copy to f. */
+    /* Build numpy array from u and result. */
+    u_array = (PyArrayObject *) PyArray_ContiguousFromObject(u_obj, NPY_DOUBLE, 0, 0);
+    if (u_array == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(u_obj, NPY_DOUBLE, 0, 0) failed.");
+        goto fail;
+    }
     result_array = (PyArrayObject *) PyArray_ContiguousFromObject(result, NPY_DOUBLE, 0, 0);
     if (result_array == NULL) {
         PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(result, NPY_DOUBLE, 0, 0) failed.");
         goto fail;
     }
 
-    /* Copy data from the result array to your C array */
-    memcpy(f, PyArray_DATA(result_array), PyArray_NBYTES(result_array));
+    /* Copy data from the result array to C array. */
+    // u' = v
+    // 0 = f(t, u, v)
+    memcpy(f, PyArray_DATA(u_array), PyArray_NBYTES(u_array));
+    memcpy(f + n, PyArray_DATA(result_array), PyArray_NBYTES(result_array));
 
     fail:
-        Py_XDECREF(y_obj);
-        Py_XDECREF(yp_obj);
+        Py_XDECREF(u_obj);
+        Py_XDECREF(v_obj);
         Py_XDECREF(result);
         Py_XDECREF(arglist);
+        Py_XDECREF(u_array);
         Py_XDECREF(result_array);
         return;
 }
@@ -131,30 +149,67 @@ void radau_jac(F_INT ldj, F_INT neqn, F_INT nlj, F_INT nuj,
              double *t, double *y, double *ydot, double *J, 
              double *rpar, F_INT *ipar){}
 
+// AM(neq, lmas)
+void radau_mas(F_INT *neqn, double *am, F_INT *lmas,
+               double *rpar, F_INT *ipar) {
+    // // allocate memory for the matrix
+    // *am = (double *)calloc(lmas * neqn, sizeof(double));
+    // if (am == NULL) {
+    //     printf("Memory allocation in radau_mas failed.\n");
+    //     return NULL;
+    // }
+
+    // // fill the diagonal with the specified values
+    // // remaining diagonal elements are already 0 due to calloc
+    // for (int i = 0; i < neqn / 2; i++) {
+    //     am[i * size + i] = 1.0; // column-major order: A(i, i) = A[i * size + i]
+    // }
+
+    // column-major ordering
+    int n = neqn / 2;
+    for (int j = 0; j < lmas; j++) {
+        for (int i = 0; i < neqn; i++) {
+            if (i == j && i < n) {
+                am[j * lmas + i] = 1.0;
+            } else {
+                am[j * lmas + i] = 0.0;
+            }
+        }
+    }
+}
+
 static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     PyObject *f_obj = NULL;
     PyObject *J_obj = Py_None;
     PyObject *t_span_obj = NULL;
-    PyObject *y_obj = NULL;
-    PyObject *yp_obj = NULL;
+    PyObject *u_obj = NULL;
+    PyObject *v_obj = NULL;
+    // PyObject *y_obj = NULL;
     PyObject *order_sol;
     PyObject *t_sol;
     PyObject *y_sol;
-    PyObject *yp_sol;
-    PyArrayObject *y_array = NULL;
-    PyArrayObject *yp_array = NULL;
+    PyArrayObject *u_array = NULL;
+    PyArrayObject *v_array = NULL;
+    // PyArrayObject *y_array = NULL;
 
     double rtol = 1.0e-3;
     double atol = 1.0e-6;
+    double h = 1e-5;
     double t, t1;
-    double *y, *yp;
+    double *u, *v, *y;
 
     int success = 1;
 
+    int n;
     int neqn;
+
+    int ijac, mljac, mujac;
+    int ijac, mljac, mujac;
+
     int jnum;
     int ninfo = 15;
+    int itol = 0;
 
     int lrwork;
     int liwork;
@@ -170,7 +225,7 @@ static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
     static char *kwlist[] = {"f", "t_span", "y0", "yp0", // mandatory arguments
                              "rtol", "atol", "J", NULL}; // optional arguments and NULL termination
     if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OOOO|ddOOO", kwlist, 
-                                     &f_obj, &t_span_obj, &y_obj, &yp_obj, // positional arguments
+                                     &f_obj, &t_span_obj, &u_obj, &v_obj, // positional arguments
                                      &rtol, &atol, &J_obj)) // optional arguments
         return NULL;
 
@@ -195,54 +250,73 @@ static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
     }
 
     // initial conditions
-    y_array = (PyArrayObject *) PyArray_ContiguousFromObject(y_obj, NPY_DOUBLE, 0, 0);
-    if (y_array == NULL) {
-        PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(y_obj, NPY_DOUBLE, 0, 0) failed");
+    u_array = (PyArrayObject *) PyArray_ContiguousFromObject(u_obj, NPY_DOUBLE, 0, 0);
+    if (u_array == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(u_obj, NPY_DOUBLE, 0, 0) failed");
         goto fail;
     }
-    if (PyArray_NDIM(y_array) > 1) {
+    if (PyArray_NDIM(u_array) > 1) {
         PyErr_SetString(PyExc_ValueError, "Initial condition y0 must be one-dimensional.");
         goto fail;
     }
-    y = (double *) PyArray_DATA(y_array);
-    neqn = PyArray_Size((PyObject *) y_array);
+    u = (double *) PyArray_DATA(u_array);
+    n = PyArray_Size((PyObject *) u_array);
+    neqn = 2 * n;
 
-    yp_array = (PyArrayObject *) PyArray_ContiguousFromObject(yp_obj, NPY_DOUBLE, 0, 0);
+    v_array = (PyArrayObject *) PyArray_ContiguousFromObject(v_obj, NPY_DOUBLE, 0, 0);
     if (yp_array == NULL) {
-        PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(yp_obj, NPY_DOUBLE, 0, 0) failed");
+        PyErr_SetString(PyExc_ValueError, "PyArray_ContiguousFromObject(v_obj, NPY_DOUBLE, 0, 0) failed");
         goto fail;
     }
-    if (PyArray_NDIM(yp_array) > 1) {
+    if (PyArray_NDIM(v_array) > 1) {
         PyErr_SetString(PyExc_ValueError, "Initial condition yp0 must be one-dimensional.");
         goto fail;
     }
-    yp = (double *) PyArray_DATA(yp_array);
-    if (!(neqn == PyArray_Size((PyObject *) yp_array))) {
+    v = (double *) PyArray_DATA(v_array);
+    if (!(n == PyArray_Size((PyObject *) v_array))) {
         PyErr_SetString(PyExc_ValueError, "Size of y0 and yp0 have to coincide.");
         goto fail;
     }
 
+    // allocate state array and fill with initial conditions
+    y = malloc(neqn * sizeof(double));
+    memcpy(y, PyArray_DATA(u_array), PyArray_NBYTES(u_array));
+    memcpy(y + n, PyArray_DATA(v_array), PyArray_NBYTES(v_array));
+
+    // decompose y = (u, v)
+    u = y;
+    v = y + n;
+
+    /* Build numpy arrays from u and v. */
+    u_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, u);
+    if (u_obj == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, u) failed.");
+        goto fail;
+    }
+    v_obj = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, v);
+    if (v_obj == NULL) {
+        PyErr_SetString(PyExc_ValueError, "PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, v) failed.");
+        goto fail;
+    }
+
     // initialize iwork and rwork
-    lrwork = 40 + (5 + 4) * neqn + pow(neqn, 2);
-    liwork = 20 + neqn;
+    // lrwork = 20 + neqn * (neqn + neqn + 3 * neqn + 12);
+    lrwork = 20 + neqn * (5 * neqn + 12);
+    liwork = 3 * neqn + 20;
 
     rwork = malloc(lrwork * sizeof(double));
     iwork = malloc(liwork * sizeof(int));
 
-    for (int i=0; i<20; i++) {
+    for (int i=0; i<liwork; i++) {
         iwork[i] = 0;
+    }
+    for (int i=0; i<lrwork; i++) {
         rwork[i] = 0.0;
     }
 
-    // initialize info
-    info = malloc(ninfo * sizeof(int));
-    for (int i=0; i<ninfo; i++) {
-        info[i] = 0;
-    }
-    // get intermediate results
-    info[2] = 1;
-    // numerical jacobian
-    info[5] = jnum;
+    // second order system
+    iwork[8] = n;
+    // iwork[9] = n; // this is implicitely assumed
 
     // set global parameters
     global_radau_params.neqn = neqn;
@@ -253,31 +327,50 @@ static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
     t_sol = PyList_New(0);
     y_sol = PyList_New(0);
     yp_sol = PyList_New(0);
-    PyList_Append(order_sol, PyLong_FromLong(1));
+    // PyList_Append(order_sol, PyLong_FromLong(1));
     PyList_Append(t_sol, PyFloat_FromDouble(t));
-    PyList_Append(y_sol, PyArray_NewCopy(y_array, NPY_ANYORDER));
-    PyList_Append(yp_sol, PyArray_NewCopy(yp_array, NPY_ANYORDER));
+    PyList_Append(y_sol, PyArray_NewCopy(u_array, NPY_ANYORDER));
+    PyList_Append(yp_sol, PyArray_NewCopy(v_array, NPY_ANYORDER));
 
-    idid = 0;
-    while (idid < 2) {
-        // call dassl solver
-        DDASSL(dassl_f, &neqn, &t, y, yp,
-            &t1, info, &rtol, &atol, &idid, 
-            rwork, &lrwork, iwork, &liwork, 
-            rpar, ipar);
+    // call radau solver
+    // void RADAU(F_INT *neq, radau_f_t *f, double *t, 
+    //         double *y, double *tout, double *h, 
+    //         double* rtol, double *atol, F_INT* itol, 
+    //         radau_jac_t *jac, F_INT *ijac /*should be boolean*/, 
+    //         F_INT *mljac /*should be boolean*/, F_INT *mujac, 
+    //         radau_mas_t *mas, F_INT *imas /*should be boolean*/, 
+    //         F_INT *mlmas /*should be boolean*/, F_INT *mumas, 
+    //         radau_solout_t *solout, F_INT *iout,
+    //         double *rwork, F_INT *lwork, F_INT *iwork, F_INT *liwork, 
+    //         double *rpar, F_INT *ipar, F_INT *idid);
+    RADAU(&neqn, radau_f, &t, y, &t1, &h, 
+          &rtol, &atol, &itol, 
+          radau_jac, &ijac, &mljac, &mujac,
+          radau_mas, &imas, & mlmas, &mumas,
+          radau_solout, &iout, 
+          rwork, &lrwork, iwork, &liwork, 
+          rpar, ipar, &idid);
 
-        // an error occured
-        if (idid < -1) {
-            success = 0;
-            break;
-        }
+    // idid = 0;
+    // while (idid < 2) {
+    //     // call dassl solver
+    //     DDASSL(dassl_f, &neqn, &t, y, yp,
+    //         &t1, info, &rtol, &atol, &idid, 
+    //         rwork, &lrwork, iwork, &liwork, 
+    //         rpar, ipar);
 
-        // store new state in solution lists
-        PyList_Append(order_sol, PyLong_FromLong(iwork[7]));
-        PyList_Append(t_sol, PyFloat_FromDouble(t));
-        PyList_Append(y_sol, PyArray_NewCopy(y_array, NPY_ANYORDER));
-        PyList_Append(yp_sol, PyArray_NewCopy(yp_array, NPY_ANYORDER));
-    }
+    //     // an error occured
+    //     if (idid < -1) {
+    //         success = 0;
+    //         break;
+    //     }
+
+    //     // store new state in solution lists
+    //     PyList_Append(order_sol, PyLong_FromLong(iwork[7]));
+    //     PyList_Append(t_sol, PyFloat_FromDouble(t));
+    //     PyList_Append(y_sol, PyArray_NewCopy(y_array, NPY_ANYORDER));
+    //     PyList_Append(yp_sol, PyArray_NewCopy(yp_array, NPY_ANYORDER));
+    // }
 
     // cleanup
     free(rwork);
@@ -285,10 +378,10 @@ static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
     Py_XDECREF(f_obj);
     Py_XDECREF(J_obj);
     Py_XDECREF(t_span_obj);
-    Py_XDECREF(y_obj);
-    Py_XDECREF(yp_obj);
-    Py_XDECREF(y_array);
-    Py_XDECREF(yp_array);
+    Py_XDECREF(u_obj);
+    Py_XDECREF(v_obj);
+    Py_XDECREF(u_array);
+    Py_XDECREF(v_array);
     
     return Py_BuildValue(
         "{s:N,s:N,s:N,s:N,s:N,s:i,s:i,s:i,s:i,s:i}",
@@ -338,10 +431,10 @@ static PyObject* radau(PyObject *self, PyObject *args, PyObject *kwargs)
         Py_XDECREF(f_obj);
         Py_XDECREF(J_obj);
         Py_XDECREF(t_span_obj);
-        Py_XDECREF(y_obj);
-        Py_XDECREF(yp_obj);
-        Py_XDECREF(y_array);
-        Py_XDECREF(yp_array);
+        Py_XDECREF(u_obj);
+        Py_XDECREF(v_obj);
+        Py_XDECREF(u_array);
+        Py_XDECREF(v_array);
         return NULL;
 }
 
