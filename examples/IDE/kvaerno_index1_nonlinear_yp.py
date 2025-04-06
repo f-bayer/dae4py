92a26a3488c8d16eddba6c07aa524c5a0b182bb0
Jonas Breuling <jonas.breuling@inm.uni-stuttgart.de>
2025-04-06 13:16:26 +0200

    Minor cleanup.

--- examples/IDE/kvaerno_index1_nonlinear_yp.py
+++ examples/IDE/kvaerno_index1_nonlinear_yp.py
@@ -1,9 +1,7 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy_dae.integrate import solve_dae
-from dae4py.irk import solve_dae_IRK_generic
-from dae4py.bdf import solve_dae_BDF
+from dae4py.irk import solve_dae_IRK
 from dae4py.butcher_tableau import radau_tableau, gauss_legendre_tableau
 
 
@@ -48,10 +46,6 @@ if __name__ == "__main__":
     # time span
     t0 = 0.1
     t1 = 1.2
-    # t0 = 0.5
-    # t1 = 0.7
-    # t0 = 0.5
-    # t1 = 0.8
     t_span = (t0, t1)
 
     # initial conditions
@@ -61,62 +55,20 @@ if __name__ == "__main__":
     # Butcher tableau
     s = 2
     tableau = radau_tableau(s)
-    # tableau = gauss_tableau(s)
+    # tableau = gauss_legendre_tableau(s)
 
     # solver settings
     h = 1e-2
     atol = rtol = 1e-6
-    sol = solve_dae_IRK_generic(
-        F, y0, yp0, t_span, h, tableau, atol=atol, rtol=rtol, newton_max_iter=10
-    )
+    sol = solve_dae_IRK(F, y0, yp0, t_span, h, tableau, atol=atol, rtol=rtol)
 
     y, yp = true_sol(sol.t)
-    # print(f"sol.t: {sol.t}")
-    # print(f"y(t0): {y[:, 0]}")
 
     fig, ax = plt.subplots()
     ax.plot(sol.t, sol.y[:, 0], "-k", label="y1")
     ax.plot(sol.t, sol.y[:, 1], "-b", label="y2")
     ax.plot(sol.t, y[0], "ok", label="y1 true")
     ax.plot(sol.t, y[1], "ob", label="y2 true")
-    ax.grid()
-    ax.legend()
-    plt.show()
-
-    exit()
-
-    # h = np.logspace(-1, -8, num=10)
-    num = 10
-    pow_max = 3
-    hs = 10 ** (-np.linspace(1.25, pow_max, num=num))
-    print(f"hs: {hs}")
-
-    errors = []
-    y_true, yp_true = true_sol(t1)
-    for h in hs:
-        sol = solve_dae_IRK_generic(
-            F, y0, yp0, t_span, h, tableau, atol=atol, rtol=rtol
-        )
-        # sol = solve_dae_BDF(F, y0, yp0, t_span, h, atol=atol, rtol=rtol)
-        t = sol.t
-        y = sol.y
-        diff = y[-1] - true_sol(t[-1])[0]
-        error = np.linalg.norm(diff)
-        errors.append(error)
-    errors = np.array(errors)
-
-    print(f"errors: {errors}")
-
-    orders = np.log(errors[:-1] / errors[1:]) / np.log(hs[:-1] / hs[1:])
-    print(f"orders: {orders}")
-
-    fig, ax = plt.subplots()
-    ax.loglog(hs, errors, "-o", label="IRK")
-    ax.loglog(hs, 1e-1 * hs ** (2 * s - 4), "-o", label=f"2 * s - 4 = {2 * s - 4}")
-    ax.loglog(hs, 1e-1 * hs ** (2 * s - 3), "-o", label=f"2 * s - 3 = {2 * s - 3}")
-    ax.loglog(hs, 1e-1 * hs ** (2 * s - 2), "-o", label=f"2 * s - 2 = {2 * s - 2}")
-    ax.loglog(hs, 1e-1 * hs ** (2 * s - 1), "-o", label=f"2 * s - 1 = {2 * s - 1}")
-    ax.loglog(hs, 1e-1 * hs ** (2 * s), "-o", label=f"2 * s = {2 * s}")
     ax.grid()
     ax.legend()
     plt.show()
